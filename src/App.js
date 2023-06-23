4af0be7158450ec0e65af06aa755abb09d5e029e
juliana_ <fjuliana6789@gmail.com>
2023-06-23 10:16:52 -0300

    Adicionando minha pasta ao repositório

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import './App.scss';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home.js';
+import Contact from './components/pages/Contact.js';
+import About from './components/pages/About.js';
+import Navbar from './components/includes/Navbar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Carrousel from "../includes/Carrousel";
+
+function Home () {
+    return(
+  
+  <div> 
+    <Carrousel/>
+<div className="container"> 
+<div className="card mt-4">
+  <div className="card-body">
+    <h2>Home</h2>
+  </div>
+</div>
+
+</div>
+</div>
+    );
+}
+
+export default Home;
